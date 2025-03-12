304cc957e69e4ff47198180d4637a4515d7103f5
RConradGe <rcgadduang@gmail.com>
2025-03-12 11:53:54 -0700

    wip: required and mounted passUserToView & isSignedIn

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require(".middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
 
 const authController = require('./controllers/auth.js');
 
@@ -28,6 +30,9 @@ app.use(
   })
 );
 
+// passUserToView comes after session middleware but before homepage
+app.use(passUserToView);
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
@@ -35,6 +40,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authController);
+
+app.use(isSignedIn);
+// this middleware rusn after auth routes - the user needs to authenticate first
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
